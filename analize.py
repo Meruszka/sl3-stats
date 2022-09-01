3e126467cf40442aa8993b39964e7e0dcc17cef6
SMerski <merski70@gmail.com>
2022-09-01 23:00:08 +0200

    ustawianie pozycji, 60%

--- analize.py
+++ analize.py
@@ -6,12 +6,35 @@ import matplotlib.pyplot as plt
 
 def get_player(name, surname, number) -> pd.DataFrame:
     df = Player(name, surname, number).from_csv()
-    df= df[df['Sety'] != 0]
-    print(df)
+    df = df[df['Sety'] != 0]
     return df
+
+def set_position(player_data):
+    #Ustala pozycje w każdym sezonie...
+    player_data['Pozycja'] = None
+    # Libero nie atakuje
+    player_data.loc[player_data['Ataki / Set'] == '0.00', 'Pozycja'] = 'Libero'
+
+    # Setter atakuje mało, serwuje
+    player_data.loc[player_data['Ataki / Set'] <= 0.50, 'Pozycja'] = 'Setter'
+
+    # Attak najwięcej atakuje
+    player_data.loc[player_data['Ataki / Set'] >= 1.00, 'Pozycja'] = 'Attack'
+    
+    # Middle najwięcej blokuje (chyba)
+    player_data.loc[player_data['Bloki / Set'] >= 0.5, 'Pozycja'] = 'Middle'
+
+    return player_data  
 
 def plot_player(player_data):
     plt.plot(player_data["Sezon"].values, player_data["Pkt/Set"].values, label='pkt/set')
+    plt.plot(player_data["Sezon"].values, player_data["Ataki / Set"].values, label='ataki/set')
+    plt.plot(player_data["Sezon"].values, player_data["Bloki / Set"].values, label='bloki/set')
+    plt.plot(player_data["Sezon"].values, player_data["Asy /  Set"].values, label='asy/set')
+    print(player_data["Sety"])
+    plt.legend()
     plt.show()
 
-plot_player(get_player('Michał', 'Niewiadomski', 25))
+p1 = get_player('Mikołaj', 'Stempin', 6)
+p1 = set_position(p1)
+plot_player(p1)
